fe956d96e3008ff19105fe5460eb458a65fae054
MM502 <MM502@CCSLAB530U09>
2025-02-27 17:33:33 +0800

    Add project files.

---
+++ Hotel Management System/Controllers/AccessController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Hotel_Management_System.Models;
+using System.Threading.Tasks;
+
+namespace Hotel_Management_System.Controllers
+{
+    public class AccessController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(VMLogin modelLogin)
+        {
+            if(modelLogin.Email == "admin55@yahoo.com" &&
+                modelLogin.Password == "123")
+            {
+                List<Claim> claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.NameIdentifier, modelLogin.Email),
+                    new Claim("OtherProperties", "ExampleRole")
+                };
+            }
+            return View();
+        }
+    }
+}
---
+++ Hotel Management System/Models/VMLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace Hotel_Management_System.Models
+{
+    public class VMLogin
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
